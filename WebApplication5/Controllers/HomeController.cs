63a2c03ec29be3c6c3414a927c0c5f2a59cf7fa7
Anatoliy Velikokhatskiy <avelikokhatskiy@legendari.guru>
2019-03-01 18:43:25 +0200

    Add project files.

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ModelState.Clear();
+            ResultsModel model = new ResultsModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(ResultsModel model)
+        {
+            ModelState.Clear();
+            model.Result = model.Bug1 + model.Bug2 + model.Bug3;
+            return View("FinalResult", model);
+        }
+
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication5/Models/ResultsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class ResultsModel
+    {
+        public int Bug1 { get; set; }
+        public int Bug2 { get; set; }
+        public int Bug3 { get; set; }
+        public int Result { get; set; }
+    }
+}
